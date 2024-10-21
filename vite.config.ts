c887f014aacfe580f18046baa945a8f10c0ed570
Viacheslav Snizhko <snegostoop@gmail.com>
2024-10-21 03:29:13 +0300

    appDirectory: ./app/website

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter({
+    appDirectory: './app/website'
+  }), tsconfigPaths()],
 });
